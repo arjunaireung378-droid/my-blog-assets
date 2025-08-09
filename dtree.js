7995d7abb594615d9d5d5f3b292497890440331f
arjunaireung378-droid <arjunaireung378@gmail.com>
2025-08-09 10:10:01 +0700

    Update dtree.js

--- dtree.js
+++ dtree.js
@@ -1,6 +1,6 @@
 /**************************************************************************
 	Copyright (c) 2001-2003 Geir Landrö (drop@destroydrop.com)
-	JavaScript Tree - www.destroydrop.coy/javascripts/tree/
+	JavaScript Tree - www.destroydrop.com/javascripts/tree/
 	Version 0.96	
 
 	This script can be used freely as long as all copyright messages are
@@ -190,3 +190,4 @@ if(!Array.prototype.pop) {
 	}
 	Array.prototype.pop = array_pop;
 }
+
